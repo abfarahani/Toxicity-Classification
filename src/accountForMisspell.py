ebfc243e5009a245d455083768a8da1724284e69
Jonathan Myers <something@somwhere.com>
2019-05-01 23:07:10 -0400

    Moving all files responsible for formating data into a location to be referenced by a general script

--- src/accountForMisspell.py
+++ src/accountForMisspell.py
@@ -2,55 +2,66 @@ import os
 import re
 from spellchecker import SpellChecker
 
-spell = SpellChecker()
+"""
+This function looks through all words in the target training file, applies
+spell check to each word, and stores the results in a targetOutputFile -- by
+default the target file is data/spellCorrections.txt
+"""
+def run(targTrainFile, targetOutputFile=""):
 
-allText = ""
+    if targetOutputFile == "":
+        targetOutputFile = os.path.dirname(os.path.abspath(__file__)) +  \
+            "/../data/spellCorrections.txt"
+    
+    spell = SpellChecker()
 
-with open("trainWordStatsFilter.csv",mode='r') as input:
-	allText = input.read()
+    allText = ""
 
-allData = {}
+    with open(targTrainFile,mode='r') as input:
+        allText = input.read()
 
-lines = allText.split("\n")
-for line in lines:
-	print(line)
-	exit(0)
-	if re.search('^,.*$',line):
-		continue
-	arr = line.split(",")
-	word = arr[0]
-	if len(word)==0:
-		continue
-	allData[word] = line
+    allData = {}
 
-#print(str(len(allData)))
-finalOutput = ""
-wordsSeen = 0
+    lines = allText.split("\n")
+    for line in lines:
+        print(line)
+        exit(0)
+        if re.search('^,.*$',line):
+            continue
+        arr = line.split(",")
+        word = arr[0]
+        if len(word)==0:
+            continue
+        allData[word] = line
 
-for key in allData:
-	wordsSeen += 1
-	if re.search('^[\d\.]+$',key):
-		continue
-	misspelled = spell.unknown([key])
-	for word in misspelled:
-		trans = spell.correction(word)
-		if word == trans:
-			# Drop from allData, it's incorrect without replacement
-			finalOutput += "del "+word+"\n"
-			#print(word)
-		elif trans in allData:
-			#print(word+" => "+trans)
-			finalOutput += key+":"+trans+"\n"
-		else:
-			#print("Many")
-			for option in spell.candidates(word):
-				if option in allData:
-					finalOutput += key+":"+option+"\n"
-					break
-	if wordsSeen % 10000 == 0:
-		print(wordsSeen)
+    
+    finalOutput = ""
+    wordsSeen = 0
+
+    for key in allData:
+        wordsSeen += 1
+        if re.search('^[\d\.]+$',key):
+            continue
+        misspelled = spell.unknown([key])
+        for word in misspelled:
+            trans = spell.correction(word)
+            if word == trans:
+                # Drop from allData, it's incorrect without replacement
+                finalOutput += "del "+word+"\n"
+                #print(word)
+            elif trans in allData:
+                #print(word+" => "+trans)
+                finalOutput += key+":"+trans+"\n"
+            else:
+                #print("Many")
+                for option in spell.candidates(word):
+                    if option in allData:
+                        finalOutput += key+":"+option+"\n"
+                        break
+        if wordsSeen % 10000 == 0:
+            print(wordsSeen)
 
 
-with open("spellCorrections.txt",mode="w") as output:
-	output.write(finalOutput)
-	output.close()
+    with open(targetOutputFile,mode="w") as output:
+        output.write(finalOutput)
+        output.close()
--- src/applyDelWordStatAndCount.py
+++ src/applyDelWordStatAndCount.py
@@ -1,39 +1,62 @@
 import os
 import re
 
-removeList = []
-allLines = ""
-
-with open("spellCorrections.txt",mode="r") as input:
-	allLines = input.read()
-
-lines = allLines.split("\n")
-for line in lines:
-	if re.search("^del\s+.*$",line):
-		line = re.sub("^del\s+","",line)
-		removeList.append(line)
-
-# Now remove those words from the train and test files
-outputLines = ""
-with open("trainWordStatsFilter.csv",mode="r") as input:
-        allLines = input.read()
-lines = allLines.split("\n")
-for line in lines:
-        word = line
-        word = re.sub("^(\w+),.*$",r"\1",line)
-        if word not in removeList:
-                outputLines += line + "\n"
-with open("trainWordStatsFilterDel.csv",mode="w") as output:
-        output.write(outputLines)
-
-outputLines = ""
-with open("testWordCountsFilter.csv",mode="r") as input:
-        allLines = input.read()
-lines = allLines.split("\n")
-for line in lines:
-        word = line
-        word = re.sub("^(\w+),.*$",r"\1",line)
-        if word not in removeList:
-                outputLines += line + "\n"
-with open("testWordCountsFilterDel.csv",mode="w") as output:
-        output.write(outputLines)
+"""
+This function looks at the output file generated from running the spell
+correction process and removes all words for which the spell checker could
+not find a rational alternative spelling
+"""
+def run(spellCorrectFiles="",trainWordInputFile="", testWordInputFile="", \
+        trainWordOutputFile="", testWordOutputFile=""):
+    removeList = []
+    allLines = ""
+    
+    if spellCorrectFiles == "":
+        spellCorrectFiles = os.path.dirname(os.path.abspath(__file__)) +  \
+            "/../data/spellCorrections.txt"
+    if trainWordInputFile == "":
+        trainWordInputFile = os.path.dirname(os.path.abspath(__file__)) +  \
+            "/../data/trainWordStatsFilter.csv"
+    if testWordInputFile == "":
+        testWordInputFile = os.path.dirname(os.path.abspath(__file__)) +  \
+            "/../data/testWordCountsFilter.csv"
+    if trainWordOutputFile == "":
+        trainWordOutputFile = os.path.dirname(os.path.abspath(__file__)) +  \
+            "/../data/trainWordStatsFilterDel.csv"
+    if testWordOutputFile == "":
+        testWordOutputFile = os.path.dirname(os.path.abspath(__file__)) +  \
+            "/../data/testWordCountsFilterDel.csv"
+    
+    with open(spellCorrectFiles,mode="r") as input:
+    	allLines = input.read()
+    
+    lines = allLines.split("\n")
+    for line in lines:
+    	if re.search("^del\s+.*$",line):
+    		line = re.sub("^del\s+","",line)
+    		removeList.append(line)
+    
+    # Now remove those words from the train and test files
+    outputLines = ""
+    with open(trainWordInputFile,mode="r") as input:
+            allLines = input.read()
+    lines = allLines.split("\n")
+    for line in lines:
+            word = line
+            word = re.sub("^(\w+),.*$",r"\1",line)
+            if word not in removeList:
+                    outputLines += line + "\n"
+    with open(trainWordOutputFile,mode="w") as output:
+            output.write(outputLines)
+    
+    outputLines = ""
+    with open(testWordInputFile,mode="r") as input:
+            allLines = input.read()
+    lines = allLines.split("\n")
+    for line in lines:
+            word = line
+            word = re.sub("^(\w+),.*$",r"\1",line)
+            if word not in removeList:
+                    outputLines += line + "\n"
+    with open(testWordOutputFile,mode="w") as output:
+            output.write(outputLines)
